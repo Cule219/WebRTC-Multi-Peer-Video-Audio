dda34aac2eb84f329fdac71a36820c2e19fcd568
Tzikas <niko.tzikas@gmail.com>
2019-12-04 14:38:45 -0500

    ok

--- server/app.js
+++ server/app.js
@@ -1,9 +1,25 @@
-var express = require('express')
-  , routes = require('./routes');
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+var cors = require('cors')
 
-var app = module.exports = express.createServer();
 
-var io = require('socket.io')(app);
+app.use(cors({
+	origin: function(origin, callback){
+		return callback(null, true);
+	},
+	optionsSuccessStatus: 200,
+	credentials: true
+}));
+
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/', function(req, res, next){
+	console.log(req.body, req.params, req.query)
+})
 
 io.on('connection', function(socket){
 	io.sockets.emit("user-joined", socket.id, io.engine.clientsCount, Object.keys(io.sockets.clients().sockets));
@@ -21,6 +37,26 @@ io.on('connection', function(socket){
 	})
 });
 
-app.listen(3000, function(){
-	console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+http.listen(3000, function(){
+  console.log('listening on *:3000');
 });
+
+// var express = require('express')
+// var cors = require('cors')
+//   //, routes = require('./routes');
+// var app = require('express')(3000);
+
+// 	// app.use(cors())
+
+	
+// 	var server = require('http').Server(app);
+// 	var io = require('socket.io')(server);
+// //var app = module.exports = express();
+
+// //var io = require('socket.io')(app);
+
+
+
+// app.listen(3000, function(){
+// 	console.log("Express server listening on port %d in %s mode", process.env);
+// });
